eb1cbca0b2fcf1bbb3d2a58192773ab87e3e4787
Lucas Class <lucasclass94@gmail.com>
2024-11-08 23:16:47 -0300

    feat: prisma new table and env

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,15 @@
-const TransactionsPage = () => {
-  return <h1>...hi!</h1>;
+import { db } from "../_lib/prisma";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <section>
+      {transactions.map((transaction) => (
+        <div key={transaction.id}>{transaction.name}</div>
+      ))}
+    </section>
+  );
 };
 
 export default TransactionsPage;
