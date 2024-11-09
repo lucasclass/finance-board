a0362b2f3121d06c6a4c20ceda237c8fc7a2f0bb
Lucas Class <lucasclass94@gmail.com>
2024-11-08 23:46:52 -0300

    feat: adding table and column components

---
+++ app/transactions/_columns/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Badge } from "@/app/_components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { CircleIcon } from "lucide-react";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+
+export const transactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => {
+      if (transaction.type === TransactionType.DEPOSIT) {
+        return (
+          <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+            <CircleIcon className="mr-2 fill-primary" size={10} />
+            Depósito
+          </Badge>
+        );
+      }
+      if (transaction.type === TransactionType.EXPENSE) {
+        return "despesa";
+      }
+      return "investimento";
+    },
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+  },
+];
