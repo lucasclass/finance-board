d609e02e6a7edfdcc18a38a1e2b1670ded7bc263
Lucas Class <lucasclass94@gmail.com>
2024-11-09 21:22:05 -0300

    feat: add badge type

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { Badge } from "@/app/_components/ui/badge";
-import { Transaction, TransactionType } from "@prisma/client";
+import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { CircleIcon } from "lucide-react";
+import BadgeType from "../_components/badge";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -16,20 +15,9 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "type",
     header: "Tipo",
-    cell: ({ row: { original: transaction } }) => {
-      if (transaction.type === TransactionType.DEPOSIT) {
-        return (
-          <Badge className="bg-muted font-bold text-primary hover:bg-muted">
-            <CircleIcon className="mr-2 fill-primary" size={10} />
-            Depósito
-          </Badge>
-        );
-      }
-      if (transaction.type === TransactionType.EXPENSE) {
-        return "despesa";
-      }
-      return "investimento";
-    },
+    cell: ({ row: { original: transaction } }) => (
+      <BadgeType transaction={transaction} />
+    ),
   },
   {
     accessorKey: "category",
